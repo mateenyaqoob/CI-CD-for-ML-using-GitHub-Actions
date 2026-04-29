e3607c43b0166505d9aadcf9344347f034b5220d
Maisum Abbas <maisum279@gmail.com>
2026-04-29 12:30:27 +0500

    Added random_state for RandomForestClassifier as well

--- src/train.py
+++ src/train.py
@@ -10,7 +10,7 @@ y = df['target']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-model = RandomForestClassifier()
+model = RandomForestClassifier(random_state=42)
 model.fit(X_train, y_train)
 
 joblib.dump(model, "models/model.pkl")
