e1b89ebd4b120cb4a179e60d59cd944a7e3fe141
Hassan-Rizwan444 <hrmalik2014@gmail.com>
2026-05-03 20:04:33 +0500

    Update test_model.py

--- test_model.py
+++ test_model.py
@@ -1,7 +1,11 @@
+import os
 import joblib
 import numpy as np
 
 def test_prediction():
+    assert os.path.exists("models/model.pkl"), "Model file not found!"
+
     model = joblib.load("models/model.pkl")
-    pred = model.predict(np.array([[5,6]]))
-    assert pred[0] in [0,1]
+    pred = model.predict(np.array([[5, 6]]))
+
+    assert pred[0] in [0, 1]
