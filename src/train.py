2b67593b3af83c1721131bf62310ce6f28230f10
umema <156839722+umema2004@users.noreply.github.com>
2026-06-08 13:38:32 +0500

    fix: correct Python version, upgrade actions, create models dir (#77)
    Co-authored-by: Mateen Yaqoob <80386974+mateenyaqoob@users.noreply.github.com>

--- src/train.py
+++ src/train.py
@@ -18,6 +18,8 @@ X_train, X_test, y_train, y_test = train_test_split(
 model = RandomForestClassifier(random_state=42)
 model.fit(X_train, y_train)
 
+import os
+os.makedirs('models', exist_ok=True)
 os.makedirs("models", exist_ok=True)
 joblib.dump(model, "models/model.pkl")
 
